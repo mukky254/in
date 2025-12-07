959257624b65bed2995f379a6a06e3e165940c66
mukky254 <muhidinyusuf06@gmail.com>
2025-12-07 18:06:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import { AuthProvider } from './context/AuthContext';
-import { Toaster } from 'react-hot-toast';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-      <Toaster position="top-right" />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
